e1b377fc7c7255e9a94ffbe99c88fbcab496d174
sumw3 <sumw3@qq.com>
2017-04-24 14:31:23 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ git pull coding master
 echo "git pull successfully"
 hexo g --d
 echo "built successfully"
+rm -rf /var/www/hexo/*
+cp -rf /root/gitpage/public/* /var/www/hexo/
+echo "copy successfully"
